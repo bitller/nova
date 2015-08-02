c657aff5b7ee1ed7c018b7f195b1c331bf4f3f71
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-02 16:28:46 +0300

    Created header and first section structure

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,7 +8,7 @@
 
 <body>
 
-<!-- Fixed navbar -->
+<!-- BEGIN Navigation bar -->
 <nav class="navbar navbar-inverse navbar-fixed-top">
     <div class="container">
         <div class="navbar-header">
@@ -18,26 +18,22 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <a class="navbar-brand" href="#">Nova</a>
+            <a class="navbar-brand" href="#">Project name</a>
         </div>
-        <div id="navbar" class="navbar-collapse collapse">
-            <ul class="nav navbar-nav navbar-right">
-                <li>
-                    <p class="navbar-btn">
-                        <a href="#" class="btn btn-default">I'm a link button!</a>
-                    </p>
-                </li>
+        <div id="navbar" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
             </ul>
         </div><!--/.nav-collapse -->
     </div>
 </nav>
+<!-- END Navigation bar -->
 
-<div class="first-section">
-    <div class="container">
-        <h2 class="text-center">Welcome</h2>
-    </div>
+<div class="container">
+    @yield('content')
 </div>
-
 </body>
 
 </html>
