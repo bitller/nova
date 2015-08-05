a8701c6a4f27edc9b8f08cb00883ea604c952680
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-06 00:13:50 +0300

    Working on login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -7,17 +7,44 @@
 </head>
 <body id="login-page">
 <div class="container">
-    <form class="col-md-4 col-md-offset-4 login-form">
-        <div class="form-group">
-            <input type="text" class="form-control" placeholder="Email" />
-        </div>
 
-        <div class="form-group">
-            <input type="password" class="form-control" placeholder="Password" />
-        </div>
+    <div class="row register-button">
+        {{--<div class="register-button">--}}
+            <a href="/register"><button class="btn btn-danger pull-right">Creaza cont</button></a>
+        {{--</div>--}}
+    </div>
 
-        <input type="submit" class="btn btn-primary">
-    </form>
+    <div class="row">
+        <!-- BEGIN Login form -->
+        <form class="col-md-4 col-md-offset-4 login-form">
+
+            <!-- BEGIN Logo -->
+            <div class="login-logo">
+                <a href="/"><h2 class="text-center">Nova</h2></a>
+            </div>
+            <!-- END Logo -->
+
+            <!-- BEGIN Email input -->
+            <div class="form-group">
+                <input type="text" class="form-control" placeholder="Email" />
+            </div>
+            <!-- END Email input -->
+
+            <!-- BEGIN Password input -->
+            <div class="form-group">
+                <input type="password" class="form-control" placeholder="Password" />
+            </div>
+            <!-- END Password input -->
+
+            <input type="submit" class="btn btn-primary btn-block" value="Conecteaza-te">
+        </form>
+        <!-- END Login form -->
+    </div>
+
+    <div class="row forgot-password">
+        <a href="/recover"><p class="text-center">Ai uitat parola?</p></a>
+    </div>
+
 </div>
 </body>
 </html>
