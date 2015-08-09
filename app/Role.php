8e1bb40fdc6be26cce6210c82a7af381aed7e4d4
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-09 12:45:03 +0300

    Created Role model

---
+++ app/Role.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Role model
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
+class Role extends Model {
+
+    /**
+     * Database table used by the model
+     *
+     * @var string
+     */
+    protected $table = 'roles';
+
+}
