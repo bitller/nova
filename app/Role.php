648151d98fec5fd463596182717e59e8dfa792c3
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-09 12:52:03 +0300

    Removed timestamps

--- app/Role.php
+++ app/Role.php
@@ -11,6 +11,13 @@ use Illuminate\Database\Eloquent\Model;
 class Role extends Model {
 
     /**
+     * Indicate if the model should be timestamped
+     *
+     * @var bool
+     */
+    public $timestamps = false;
+
+    /**
      * Database table used by the model
      *
      * @var string
