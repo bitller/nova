43bcb2c379bd1db63609beed361bd61622df18e9
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-11 14:41:27 +0300

    Added comments and disabled timestamps

--- app/LogType.php
+++ app/LogType.php
@@ -4,6 +4,17 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * LogType model
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
 class LogType extends Model {
-    //
+
+    /**
+     * Disable timestamps for this model
+     *
+     * @var bool
+     */
+    public $timestamps = false;
 }
