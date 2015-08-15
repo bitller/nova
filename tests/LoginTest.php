8ea1f4fde977c19b8d4bf0ae65c2b03c5c0a5885
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-15 22:53:38 +0300

    Created new test file

---
+++ tests/LoginTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * LoginTest
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
+class LoginTest extends TestCase {
+
+    public function loginWithValidCredentials() {
+        //
+    }
+
+}
