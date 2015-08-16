02db8a6cb900750f34bb423e18b47e33e1bce2e7
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-16 12:32:07 +0300

    Created LoginTest

--- tests/LoginTest.php
+++ tests/LoginTest.php
@@ -7,8 +7,68 @@
  */
 class LoginTest extends TestCase {
 
-    public function loginWithValidCredentials() {
-        //
+    /**
+     * Login button text
+     *
+     * @var string
+     */
+    private $loginButton = 'Conecteaza-te';
+
+    /**
+     * Submit login form with valid credentials
+     */
+    public function testLoginWithValidCredentials() {
+
+        $validEmail = 'alexandru.bugarin@gmail.com';
+        $validPassword = '123456';
+
+        $this->visit('/login')
+            ->type($validEmail, 'email')
+            ->type($validPassword, 'password')
+            ->press($this->loginButton)
+            ->seePageIs('/bills');
+    }
+
+    /**
+     * Submit login form with all inputs empty
+     */
+    public function testLoginWithEmptyForm() {
+
+        $this->visit('/login')
+            ->press($this->loginButton)
+            ->see('The email field is required.');
+
+    }
+
+    public function testLoginWithoutEmail() {
+
+        $this->visit('/login')
+            ->type(str_random(10), 'password')
+            ->press($this->loginButton)
+            ->see('The email field is required.');
+
+    }
+
+    /**
+     * Submit login form without password input
+     */
+    public function testLoginWithoutPassword() {
+
+        $user = factory(App\User::class)->make();
+
+        $this->visit('/login')
+            ->type($user->email, 'email')
+            ->press($this->loginButton)
+            ->see('The password field is required.');
+
+    }
+
+    public function testCreateAccountButton() {
+
+        $this->visit('/login')
+            ->click('Creaza cont')
+            ->seePageIs('/register');
+
     }
 
 }
