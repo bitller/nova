04b257beb572d1da2deaa38ac517cf7689e0c30b
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-16 18:23:37 +0300

    Translated to romain

---
+++ resources/lang/ro/passwords.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'password' => 'Parola trebuie să aibe cel puțin 6 caractere și să coincidă cu confirmarea.',
+    'user' => 'Această adresă de email nu aparține nici unui utilizator.',
+    'token' => 'Token-ul pentru resetarea parolei este invalid.',
+    'sent' => 'Un email care conține link-ul pentru resetarea parolei a fost trimis!',
+    'reset' => 'Parola ta a fost resetată!',
+];
