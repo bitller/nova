ca1f1e99b6e98bc63bc49b028e46792918500521
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-16 11:21:47 +0300

    Commented Role helper

--- app/Helpers/Roles.php
+++ app/Helpers/Roles.php
@@ -4,26 +4,67 @@ namespace App\Helpers;
 
 use App\Role;
 
+/**
+ * Role helper
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
 class Roles {
 
+    /**
+     * Administrator level
+     *
+     * @var string
+     */
     protected $adminLevel = '1';
 
+    /**
+     * Moderator level
+     *
+     * @var string
+     */
     protected $moderatorLevel = '2';
 
+    /**
+     * Normal user level
+     *
+     * @var string
+     */
     protected $userLevel = '3';
 
+    /**
+     * Get admin role id
+     *
+     * @return int
+     */
     public function getAdminRoleId() {
         return $this->getRoleId($this->adminLevel);
     }
 
+    /**
+     * Get moderator role id
+     *
+     * @return int
+     */
     public function getModeratorRoleId() {
         return $this->getRoleId($this->moderatorLevel);
     }
 
+    /**
+     * Get normal user role id
+     *
+     * @return int
+     */
     public function getUserRoleId() {
         return $this->getRoleId($this->userLevel);
     }
 
+    /**
+     * Get role id for the given $level
+     *
+     * @param int $level
+     * @return int
+     */
     private function getRoleId($level) {
         return Role::where('level', $level)->value('id');
     }
