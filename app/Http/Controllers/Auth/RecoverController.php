aee7d1e0a262471bdd006fcf7dbdfa36d142361f
mezuky04 <alexandru.bugarin@gmail.com>
2015-08-16 12:31:32 +0300

    Added new controllers

--- app/Http/Controllers/Auth/RecoverController.php
+++ app/Http/Controllers/Auth/RecoverController.php
@@ -4,10 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 
-class RegisterController extends Controller {
+class RecoverController extends Controller {
 
     public function index() {
-        return ('this is the register page');
+        //
     }
 
 }
