b137418c525fd3841c8a203002d1ea0c773d7a3d
mezuky04 <alexandru.bugarin@gmail.com>
2015-09-12 21:53:55 +0300

    Implemented Translations library

---
+++ resources/assets/js/custom-libs/translations.js
@@ -0,0 +1,56 @@
+/**
+ * Handle work with translations.
+ *
+ * @type {{get: Function}}
+ */
+var Translation = {
+
+    /**
+     * Get common translations.
+     *
+     * @param attribute
+     * @returns {*|jQuery}
+     */
+    common: function(attribute) {
+        return this.get('#common-trans', attribute);
+    },
+
+    /**
+     * Get client page translations.
+     *
+     * @param attribute
+     * @returns {*|jQuery}
+     */
+    client: function(attribute) {
+        return this.get('#client-trans', attribute);
+    },
+
+    /**
+     * Get products page translations.
+     *
+     * @param attribute
+     * @returns {*|jQuery}
+     */
+    products: function(attribute) {
+        return this.get('#product-trans', attribute);
+    },
+
+    /**
+     * Get my products page translations.
+     *
+     * @param attribute
+     * @returns {*|jQuery}
+     */
+    myProducts: function(attribute) {
+        return this.get('#my-products-trans', attribute);
+    },
+
+    /**
+     * @param pageSelector
+     * @param attribute
+     * @returns {*|jQuery}
+     */
+    get: function(pageSelector, attribute) {
+        return $(pageSelector).attr(attribute);
+    }
+};
