5d4a2a9b5b2fc581b712e26453f00a51f1dad896
mezuky04 <alexandru.bugarin@gmail.com>
2015-09-20 18:34:17 +0300

    Added comments

--- app/Helpers/TestUrlBuilder.php
+++ app/Helpers/TestUrlBuilder.php
@@ -2,8 +2,21 @@
 
 namespace App\Helpers;
 
+/**
+ * Build urls used for get requests
+ *
+ * @author Alexandru Bugairn <alexandru.bugarin@gmail.com>
+ */
 class TestUrlBuilder {
 
+    /**
+     * Build url used to delete a product from a bill.
+     *
+     * @param int $billId
+     * @param int $productId
+     * @param string $productCode
+     * @return string
+     */
     public static function deleteBillProduct($billId, $productId, $productCode) {
         return '/bills/' . $billId . '/delete/' . $productId . '/' . $productCode;
     }
