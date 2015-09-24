df9e69a09af6d5ac07e255d48a20a426a400ddbf
mezuky04 <alexandru.bugarin@gmail.com>
2015-09-24 17:13:58 +0300

    Added edit product discount route

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -33,6 +33,7 @@ Route::group(['prefix' => 'bills'], function() {
     Route::post('/{billId}/edit-page', 'BillsController@editPage');
     Route::post('/{billId}/edit-quantity', 'BillsController@editQuantity');
     Route::post('/{billId}/edit-price', 'BillsController@editPrice');
+    Route::post('/{billId}/edit-discount', 'BillsController@editDiscount');
 });
 
 Route::group(['prefix' => 'clients'], function() {
