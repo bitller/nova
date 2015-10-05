fff1a26fa47940ba8b9ce0f6630b067c6f76190e
mezuky04 <alexandru.bugarin@gmail.com>
2015-10-05 21:37:30 +0300

    Now data using application products can be generated

--- tests/bills/BaseTest.php
+++ tests/bills/BaseTest.php
@@ -10,15 +10,22 @@ class BaseTest extends Testcase {
     /**
      * Generate user, client, bill, product and bill product.
      *
+     * @param bool $useApplicationProduct
      * @return array
      */
-    protected function generateData() {
+    protected function generateData($useApplicationProduct = false) {
 
         $user = factory(App\User::class)->create();
         $client = $user->clients()->save(factory(App\Client::class)->make());
         $bill = $user->bills()->save(factory(App\Bill::class)->make(['client_id' => $client->id]));
-        $product = $user->products()->save(factory(App\Product::class)->make());
-        $billProduct = $bill->products()->save(factory(App\BillProduct::class)->make(['product_id' => $product->id]));
+
+        if ($useApplicationProduct) {
+            $product = factory(App\ApplicationProduct::class)->create();
+            $billProduct = $bill->applicationProducts()->save(factory(App\BillApplicationProduct::class)->make(['product_id' => $product->id]));
+        } else {
+            $product = $user->products()->save(factory(App\Product::class)->make());
+            $billProduct = $bill->products()->save(factory(App\BillProduct::class)->make(['product_id' => $product->id]));
+        }
 
         return [
             'user' => $user,
