cce615e13fb7a0529d50725bb8cbec7c3427ea10
mezuky04 <alexandru.bugarin@gmail.com>
2015-10-24 15:16:54 +0300

    Added rules and authorization

---
+++ app/Http/Requests/Settings/EditUserPasswordRequest.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Requests\Settings;
+
+use App\Http\Requests\AjaxRequest;
+use Illuminate\Contracts\Auth\Guard;
+
+/**
+ * Authorize and validate EditUserPasswordRequest
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
+class EditUserPasswordRequest extends AjaxRequest {
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @param Guard $auth
+     * @return bool
+     */
+    public function authorize(Guard $auth) {
+        return $auth->check();
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules() {
+        return [
+            'password' => ['required', 'between:6,250'],
+            'new_password' => ['required', 'between:6,250', 'confirmed']
+        ];
+    }
+
+}
