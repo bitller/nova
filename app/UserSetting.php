43f249568fdadfc6900b5d410c19cd15178a5146
mezuky04 <alexandru.bugarin@gmail.com>
2015-10-24 15:03:36 +0300

    Added comments

--- app/UserSetting.php
+++ app/UserSetting.php
@@ -4,8 +4,19 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * UserSetting model
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
 class UserSetting extends Model {
 
+    /**
+     * Return fields  allowed to be displayed public.
+     *
+     * @param $query
+     * @return mixed
+     */
     public function scopePublic($query) {
         return $query->select('displayed_bills', 'displayed_clients', 'displayed_products', 'displayed_custom_products', 'language');
     }
