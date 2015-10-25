67d30128fe1d6783202eb3007654fe4e1084e0f6
mezuky04 <alexandru.bugarin@gmail.com>
2015-10-25 21:30:48 +0200

    Created default settings table

---
+++ database/migrations/2015_10_25_191059_create_user_default_settings_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+/**
+ * CreateUserDefaultSettingsTable migration
+ *
+ * @author Alexandru Bugarin <alexandru.bugarin@gmail.com>
+ */
+class CreateUserDefaultSettingsTable extends Migration {
+
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('user_default_settings', function(Blueprint $table) {
+
+            $table->smallIncrements('id');
+            $table->tinyInteger('displayed_bills')->default(10);
+            $table->tinyInteger('displayed_clients')->default(10);
+            $table->tinyInteger('displayed_products')->default(10);
+            $table->tinyInteger('displayed_custom_products')->default(10);
+            $table->smallInteger('language_id')->unsigned();
+
+            $table->foreign('language_id')->references('id')->on('languages')->onUpadate('cascade')->onDelete('cascade');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('user_default_settings');
+    }
+}
