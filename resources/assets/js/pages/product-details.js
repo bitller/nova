c1fc351a0371edacfd4591b24f908d861dbdd425
mezuky04 <alexandru.bugarin@gmail.com>
2015-10-28 21:34:17 +0200

    Commented and styled code

--- resources/assets/js/pages/product-details.js
+++ resources/assets/js/pages/product-details.js
@@ -1,19 +1,38 @@
+/**
+ * Handle product details page.
+ */
 new Vue({
+
+    /**
+     * Target element.
+     */
     el: '#product-details',
 
+    /**
+     * Called on ready.
+     */
     ready: function() {
         this.getData();
     },
 
+    /**
+     * Available methods.
+     */
     methods: {
+
+        /**
+         * Get product data.
+         */
         getData: function() {
 
             Alert.loader();
 
             this.$http.get('/product-details/' + $('#product-details').attr('product-code') + '/get', function(response) {
+
                 this.$set('product', response);
                 this.$set('loaded', true);
                 Alert.close();
+
             }).error(function(response) {
 
                 if (!response.message) {
