9334580843f6737e79c4cf4131fbf9ed5b603300
mezuky04 <alexandru.bugarin@gmail.com>
2015-11-20 10:26:41 +0200

    Changed validation rules orer

--- app/Http/Requests/Bill/AddProductRequest.php
+++ app/Http/Requests/Bill/AddProductRequest.php
@@ -29,11 +29,11 @@ class AddProductRequest extends AjaxRequest {
      */
     public function rules() {
         return [
-            'product_code' => ['required', 'digits:5'],
+            'product_quantity' => ['numeric', 'between:1,999'],
+            'product_discount' => ['numeric', 'between:0,100'],
             'product_page' => ['numeric', 'between:1,2000'],
             'product_price' => ['required', 'numeric', 'between:0,9999'],
-            'product_discount' => ['numeric', 'between:0,100'],
-            'product_quantity' => ['numeric', 'between:1,999']
+            'product_code' => ['required', 'digits:5'],
         ];
     }
 
