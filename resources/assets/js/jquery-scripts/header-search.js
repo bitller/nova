7963682ec2a316a74adba6f77ca580d85acba008
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-10 15:28:31 +0200

    Wokring on some bug fixes

--- resources/assets/js/jquery-scripts/header-search.js
+++ resources/assets/js/jquery-scripts/header-search.js
@@ -1,5 +1,61 @@
 $(document).ready(function() {
 
+    // Instantiate the Bloodhound suggestion engine
+    var products = new Bloodhound({
+        datumTokenizer: function (datum) {
+            return Bloodhound.tokenizers.whitespace(datum.value);
+        },
+        queryTokenizer: Bloodhound.tokenizers.whitespace,
+        remote: {
+            ajax: {
+                beforeSend: function(xhr) {
+                    $('.product-code i').show();
+                },
+                complete: function() {
+                    $('.product-code i').hide();
+                }
+            },
+            cache: false,
+            url: 'http://localhost:8888/bills/11/suggest-products?product_code=',
+            replace: function() {
+                var url = 'http://localhost:8888/bills/11/suggest-products?product_code=';
+                if ($('#product-code').val()) {
+                    url += encodeURIComponent($('#product-code').val())
+                }
+                return url;
+            },
+            filter: function (products) {
+                // Map the remote source JSON array to a JavaScript object array
+                return $.map(products, function (product) {
+                    return {
+                        value: product.code,
+                        display: product.code + ' - ' + product.name
+                    };
+                });
+            }
+        }
+    });
+
+    // Initialize the Bloodhound suggestion engine
+    products.initialize();
+
+    var input = $('.twitter-typeahead');
+
+    // Instantiate the Typeahead UI
+    input.typeahead(null, {
+        displayKey: 'value',
+        source: products.ttAdapter(),
+        templates: {
+            suggestion: function(product) {
+                return '<p>' + product.display + '</p>'
+            }
+        }
+    });
+
+    $('#payment-term').datepicker({dateFormat: 'yy-m-d'});
+
+    $('[data-toggle="tooltip"]').tooltip();
+
     // Search engine for client suggestions
 
     // Instantiate the Bloodhound suggestion engine
