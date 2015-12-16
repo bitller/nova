3fda1b1429c1699dcc3825db2e6fc94bcfc084ab
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-16 08:23:06 +0200

    Centered 'My products' table text

--- resources/views/my-products.blade.php
+++ resources/views/my-products.blade.php
@@ -25,16 +25,16 @@
                 <table class="table table-bordered">
                     <thead>
                     <tr>
-                        <th>{{ trans('common.product_code') }}</th>
-                        <th>{{ trans('common.product_name') }}</th>
-                        <th>{{ trans('my_products.delete_product') }}</th>
+                        <th class="text-center">{{ trans('common.product_code') }}</th>
+                        <th class="text-center">{{ trans('common.product_name') }}</th>
+                        <th class="text-center">{{ trans('my_products.delete_product') }}</th>
                     </tr>
                     </thead>
                     <tbody>
                     <tr v-repeat="product in myProducts.data">
-                        <td class="vert-align"><a href="/product-details/@{{ product.code }}">@{{ product.code }}</a></td>
-                        <td class="vert-align"><a href="/product-details/@{{ product.code }}">@{{ product.name }}</a></td>
-                        <td class="vert-align"><button class="btn btn-danger" v-on="click: deleteMyProduct(product.id, myProducts.current_page, myProducts.to-myProducts.from)">{{ trans('common.delete') }}</button></td>
+                        <td class="text-center vert-align"><a href="/product-details/@{{ product.code }}">@{{ product.code }}</a></td>
+                        <td class="text-center vert-align"><a href="/product-details/@{{ product.code }}">@{{ product.name }}</a></td>
+                        <td class="text-center vert-align"><button class="btn btn-danger" v-on="click: deleteMyProduct(product.id, myProducts.current_page, myProducts.to-myProducts.from)">{{ trans('common.delete') }}</button></td>
                     </tr>
                     </tbody>
                 </table>
