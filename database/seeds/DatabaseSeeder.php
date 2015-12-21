c75d7eb0a7c4e6cb9704f6e574163eb54d462dbc
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-21 13:28:38 +0200

    Added new seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder {
         Model::unguard();
 
         $this->call(ActionTableSeeder::class);
+        $this->call(LanguageTableSeeder::class);
         $this->call(UserTableSeeder::class);
 
         Model::reguard();
