fcff14927bf75849f8483cd639d7ce5b47ae1449
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-22 01:09:41 +0200

    Changed language seed

--- database/seeds/LanguageTableSeeder.php
+++ database/seeds/LanguageTableSeeder.php
@@ -6,6 +6,7 @@ class LanguageTableSeeder extends Seeder {
 
     public function run() {
         factory(App\Language::class)->create(['key' => 'en', 'language' => 'English']);
+        factory(App\Language::class)->create(['key' => 'ro', 'language' => 'Romana']);
     }
 
 }
