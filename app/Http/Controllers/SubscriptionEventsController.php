2cf64fcb746d89dcdeb68cbaf8c4cfb2bcd1f4aa
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-23 17:16:58 +0200

    Created webhook model and migration

--- app/Http/Controllers/SubscriptionEventsController.php
+++ app/Http/Controllers/SubscriptionEventsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Subscription;
+use App\Webhook;
 use Illuminate\Http\Request;
 
 /**
@@ -12,9 +13,7 @@ use Illuminate\Http\Request;
  */
 class SubscriptionEventsController extends BaseController {
 
-    public function index() {
-        $request = new Request();
-
+    public function index(Request $request) {
         // Handle case when subscription is active
         if ($request->get('status') === 'active') {
             Subscription::where('paymill_subscription_id' === $request->get('id'))->update([
@@ -22,5 +21,8 @@ class SubscriptionEventsController extends BaseController {
                 'waiting_for_paymill' => 0
             ]);
         }
+        $webhook = new Webhook();
+        $webhook->status = $request->get('status');
+        $webhook->save();
     }
 }
---
+++ database/migrations/2015_12_23_150438_create_webhooks_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWebhooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::create('webhooks', function(Blueprint $table) {
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::drop('webhooks');
+    }
+}
