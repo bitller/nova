052a0e1bf12e79d756f926ee73d2e2b9f44899fd
mezuky04 <alexandru.bugarin@gmail.com>
2015-12-23 14:49:27 +0200

    Commented langauges table seed

--- database/seeds/LanguageTableSeeder.php
+++ database/seeds/LanguageTableSeeder.php
@@ -1,13 +1,19 @@
 <?php
 
 use Illuminate\Database\Seeder;
-use Faker\Factory as Faker;
 
+/**
+ * Seeds languages table.
+ *
+ * @author Alexandru Bguarin <alexandru.bugarin@gmail.com>
+ */
 class LanguageTableSeeder extends Seeder {
 
+    /**
+     * Run table seeds.
+     */
     public function run() {
         factory(App\Language::class)->create(['key' => 'en', 'language' => 'English']);
         factory(App\Language::class)->create(['key' => 'ro', 'language' => 'Romana']);
     }
-
 }
