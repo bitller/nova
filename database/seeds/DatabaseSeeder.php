4515678f01ac3626c27ff33171555f68dd991004
mezuky04 <alexandru.bugarin@gmail.com>
2016-01-06 12:22:29 +0200

    Commented not used seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder {
         $this->call(ActionTableSeeder::class);
         $this->call(LanguageTableSeeder::class);
         $this->call(RoleTableSeeder::class);
-        $this->call(OfferTableSeeder::class);
+//        $this->call(OfferTableSeeder::class);
         $this->call(UserTableSeeder::class);
 
         Model::reguard();
