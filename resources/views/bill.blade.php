d9361b0218ddccb2589a0459ea8906232951ec25
mezuky04 <alexandru.bugarin@gmail.com>
2016-01-18 16:27:01 +0200

    Syntax error

--- resources/views/bill.blade.php
+++ resources/views/bill.blade.php
@@ -23,7 +23,7 @@
 
                 <div class="text-center bill-title-print">
                     <span>
-                        @{{ bill.data.client_name }} - {{ trans('bill.order') }} @{{ bill.data.campaign_order }} {{ tarns('bill.from_campaign') }} @{{ bill.data.campaign_number }}/@{{ bill.data.campaign_year }}
+                        @{{ bill.data.client_name }} - {{ trans('bill.order') }} @{{ bill.data.campaign_order }} {{ trans('bill.from_campaign') }} @{{ bill.data.campaign_number }}/@{{ bill.data.campaign_year }}
                     </span>
                 </div>
 
