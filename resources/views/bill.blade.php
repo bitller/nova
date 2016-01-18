a23608a748e78439d465c6c734ec898b155fc98d
mezuky04 <alexandru.bugarin@gmail.com>
2016-01-18 21:39:36 +0200

    Styling printed bill

--- resources/views/bill.blade.php
+++ resources/views/bill.blade.php
@@ -168,8 +168,8 @@
             <div class="alert alert-info" v-show="!bill.total">{{ trans('bill.empty_bill') }}</div>
 
             <div class="printed-details">
-                <span>1st detail</span>
-                <span>2nd detail</span>
+                <span class="left">1st detail</span>
+                <span class="center">2nd detail</span>
                 <span>3th detail</span>
             </div>
 
