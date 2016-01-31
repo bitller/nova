070c47404b50a842d90fc6a2c655017fa8d6e4b2
mezuky04 <alexandru.bugarin@gmail.com>
2016-01-31 08:31:07 +0200

    Implemented 'Use current campaign' and 'Auto determine order number' features

--- resources/views/includes/modals/create-bill.blade.php
+++ resources/views/includes/modals/create-bill.blade.php
@@ -31,15 +31,6 @@
                 </div>
                 <!-- END Client name -->
 
-                {{--<!-- BEGIN Campaign order number -->--}}
-                {{--<div role="form" class="col-md-12">--}}
-                    {{--<div class="form-group has-feedback">--}}
-                        {{--<label for="campaign-order-number">{{ trans('bills.campaign_order_number') }} &nbsp; <span class="badge" data-toggle="tooltip" data-placement="right" title="{{ trans('bills.campaign_order_number_description') }}">?</span> :</label>--}}
-                        {{--<input v-on="keyup:createBill | ley 13" type="text" id="campaign-order-number" class="form-control" v-model="campaign_order" placeholder="1">--}}
-                    {{--</div>--}}
-                {{--</div>--}}
-                {{--<!-- END Campaign order number -->--}}
-
                 <!-- BEGIN Use current campaign -->
                 <div class="checkbox col-md-12">
                     <label><input type="checkbox" checked="use_current_campaign" v-model="use_current_campaign" />{{ trans('bills.use_current_campaign', ['current_campaign_number' => \App\Helpers\Campaigns::current()->number, 'current_campaign_year' => \App\Helpers\Campaigns::current()->year]) }}</label>
@@ -52,39 +43,16 @@
                     <div class="form-group col-md-4">
                         <label for="campaign-year">{{ trans('bills.campaign_year') }}</label>
                         <select class="form-control" id="campaign-year" v-model="campaign_year">
-                            <option>2015</option>
-                            <option>2016</option>
-                            <option>2017</option>
+                            <option selected="selected">2016</option>
                         </select>
                     </div>
                     <div class="form-group col-md-4">
                         <label for="campaign-number">{{ trans('bills.campaign_number') }}</label>
                         <select class="form-control" id="campaign-number" v-model="campaign_number">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
-                            <option>6</option>
-                            <option>7</option>
-                            <option>8</option>
-                            <option>9</option>
-                            <option>10</option>
-                            <option>11</option>
-                            <option>12</option>
-                            <option>13</option>
-                            <option>14</option>
-                            <option>15</option>
-                            <option>16</option>
-                            <option>17</option>
-                        </select>
-                    </div>
-                    <div class="form-group col-md-4">
-                        <label for="campaign-order">{{ trans('bills.campaign_order') }}</label>
-                        <select class="form-control" id="campaign-order" v-model="campaign_order">
-                            <option>1</option>
-                            <option>2</option>
-                            <option>3</option>
-                            <option>4</option>
+                            <option selected="selected">1</option>
+                            @for ($i = 2; $i <= 17; $i++)
+                                <option>{{ $i }}</option>
+                            @endfor
                         </select>
                     </div>
                 </div>
