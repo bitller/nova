28c3ea0a31bc3f59eab730cfac44711fe5cc408f
mezuky04 <alexandru.bugarin@gmail.com>
2016-03-10 20:13:40 +0200

    Basic about page

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,6 +1,14 @@
 @extends('layout.index')
 @section('content')
-    About page content goes here
+    <div class="well custom-well">
+        <h3 class="grey-text">What is Nova?</h3>
+        <h5 class="grey-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis massa tempus, facilisis ipsum at, tempus elit. Praesent iaculis arcu sit amet arcu ullamcorper efficitur. Ut at magna tincidunt, blandit ligula quis, placerat est. Aliquam ac porttitor lacus, in facilisis turpis. </h5>
+
+        <div class="divider"></div>
+        <h3 class="grey-text">Who is the team behind?</h3>
+        <h5 class="grey-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam quis massa tempus, facilisis ipsum at, tempus elit. Praesent iaculis arcu sit amet arcu ullamcorper efficitur. Ut at magna tincidunt, blandit ligula quis, placerat est. Aliquam ac porttitor lacus, in facilisis turpis. </h5>
+    </div>
+
 @endsection
 
 @section('scripts')
